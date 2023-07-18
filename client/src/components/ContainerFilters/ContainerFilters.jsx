468a9859b86adca2287c6d330a16803dbd796733
Alejo <jubany4@gmail.com>
2023-07-18 15:21:51 -0300

    containerFilters end

--- client/src/components/ContainerFilters/ContainerFilters.jsx
+++ client/src/components/ContainerFilters/ContainerFilters.jsx
@@ -1,39 +1,31 @@
-import React from 'react'
-import SearchBar from '../SearchBar/SearchBar'
-import OrderByAlphabet from '../Filters/OrderByAlphabet'
-import OrderByPoblation from '../Filters/OrderByPoblation'
-import style from "./ContainerFilters.module.css"
-import { FilterByContinent } from '../Filters/FilterByContinent'
-import FilterByCreate from '../Filters/FilterByCreate'
-
+import React from 'react';
+import OrderByAlphabet from '../Filters/OrderByAlphabet';
+import OrderByPoblation from '../Filters/OrderByPoblation';
+import style from './ContainerFilters.module.css';
+import { FilterByContinent } from '../Filters/FilterByContinent';
+import FilterByCreate from '../Filters/FilterByCreate';
 
 const ContainerFilter = () => {
   return (
     <>
-        <section className={style.containerFilter}>
-            <div>
-              <h4>Search</h4>
-              <div>
-                <SearchBar/>
-              </div>
-            </div>
-            <div>
-              <h4>Filter</h4>
-              <div>  
-                   <FilterByContinent/>
-                   <FilterByCreate/>
-              </div>
-            </div>
-            <div>
-              <h4>Sort</h4>
-              <div>
-                <OrderByAlphabet/>
-                <OrderByPoblation/>
-              </div>
-            </div>
-        </section>
+      <section className={`${style.containerFilter} ${style.centered}`}>
+        <div>
+        </div>
+        <div className={style.filterSection}>
+          <div className={style.filterContainer}>
+            <FilterByContinent />
+            <FilterByCreate />
+          </div>
+        </div>
+        <div className={style.sortSection}>
+          <div className={style.sortContainer}>
+            <OrderByAlphabet />
+            <OrderByPoblation />
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default ContainerFilter;

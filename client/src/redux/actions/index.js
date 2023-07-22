1cc57b3bd8d36b2245d4c77770ff7997d48c9f03
Alejo <jubany4@gmail.com>
2023-07-22 12:49:51 -0300

    cambio de fetch a axios

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,67 +1,79 @@
-import axios from "axios"
-export const GET_ALL_COUNTRIES = "GET_ALL_COUNTRIES"
-export const GET_COUNTRY = "GET_COUNTRY"
-export const GET_COUNTRY_BY_NAME = "GET_COUNTRY_BY_NAME"
-export const CREATE_ACTIVITY = "CREATE_ACTIVITY"
-export const ORDER_BY_ALPHABET = "ORDER_BY_ALPHABET"
-export const ORDER_BY_POBLATION = "ORDER_BY_POBLATION"
-export const FILTER_BY_CREATE = "FILTER_BY_CREATE"
-export const GET_ACTIVITIES = "GET_ACTIVITIES"
-export const FILTER_BY_CONTINENT = "FILTER_BY_CONTINENT"
-export const CLEAN_STATE_COUNTRY_DETAIL = "CLEAN_STATE_COUNTRY_DETAIL"
+import axios from "axios";
+
+export const GET_ALL_COUNTRIES = "GET_ALL_COUNTRIES";
+export const GET_COUNTRY = "GET_COUNTRY";
+export const GET_COUNTRY_BY_NAME = "GET_COUNTRY_BY_NAME";
+export const CREATE_ACTIVITY = "CREATE_ACTIVITY";
+export const ORDER_BY_ALPHABET = "ORDER_BY_ALPHABET";
+export const ORDER_BY_POBLATION = "ORDER_BY_POBLATION";
+export const FILTER_BY_CREATE = "FILTER_BY_CREATE";
+export const GET_ACTIVITIES = "GET_ACTIVITIES";
+export const FILTER_BY_CONTINENT = "FILTER_BY_CONTINENT";
+export const CLEAN_STATE_COUNTRY_DETAIL = "CLEAN_STATE_COUNTRY_DETAIL";
 
 export const getAllCountries = () => async dispatch => {
-    return fetch("/all")
-            .then(response => response.json())
-            .then(data => dispatch({type: GET_ALL_COUNTRIES, payload: data}))
+    try {
+        const response = await axios.get("/all");
+        dispatch({ type: GET_ALL_COUNTRIES, payload: response.data });
+    } catch (error) {
+        // Handle error if needed
+    }
 };
 
 export const getCountry = (id) => async dispatch => {
-    return fetch(`/country/${id}`)
-            .then(response => response.json())
-            .then(data => dispatch({type: GET_COUNTRY, payload: data}))
+    try {
+        const response = await axios.get(`/country/${id}`);
+        dispatch({ type: GET_COUNTRY, payload: response.data });
+    } catch (error) {
+        // Handle error if needed
+    }
 };
 
 export const getCountryByName = (name) => async dispatch => {
-    return fetch(`/all?name=${name}`)
-    .then(response=> response.json())
-    .then(data=> dispatch({type:GET_COUNTRY_BY_NAME, payload: data}))
+    try {
+        const response = await axios.get(`/all?name=${name}`);
+        dispatch({ type: GET_COUNTRY_BY_NAME, payload: response.data });
+    } catch (error) {
+        // Handle error if needed
+    }
 };
 
 export const createActivity = (data) => async dispatch => {
-    try {  
-        const response = await axios.post("/createActivity", data)
-        return dispatch({type: CREATE_ACTIVITY, payload: response.data})
+    try {
+        const response = await axios.post("/createActivity", data);
+        dispatch({ type: CREATE_ACTIVITY, payload: response.data });
     } catch (error) {
-        return dispatch({type: CREATE_ACTIVITY, payload: error})
+        dispatch({ type: CREATE_ACTIVITY, payload: error });
     }
-
 };
 
 export const orderByAlphabet = (type) => {
-    return {type:ORDER_BY_ALPHABET, payload: type}
+    return { type: ORDER_BY_ALPHABET, payload: type };
 };
 
 export const cleanDetailCountry = () => {
-    return {type:CLEAN_STATE_COUNTRY_DETAIL}
+    return { type: CLEAN_STATE_COUNTRY_DETAIL };
 };
 
 export const orderByPoblation = (type) => {
-    return {type:ORDER_BY_POBLATION, payload:type}
+    return { type: ORDER_BY_POBLATION, payload: type };
 };
 
 export const filterByCreate = (type) => {
-    return {type: FILTER_BY_CREATE, payload:type }
-} ;
+    return { type: FILTER_BY_CREATE, payload: type };
+};
 
 export function getAllActivities() {
     return async (dispatch) => {
-      const act = await axios("/allActivities");
-      return dispatch({ type: GET_ACTIVITIES, payload: act.data });
+        try {
+            const response = await axios.get("/allActivities");
+            dispatch({ type: GET_ACTIVITIES, payload: response.data });
+        } catch (error) {
+            // Handle error if needed
+        }
     };
-  };
+};
 
 export function filterByContinent(payload) {
     return { type: FILTER_BY_CONTINENT, payload };
 };
-

f28122ecbfcabffd24d4cdf07d2887c4999ba1cc
Alejo <jubany4@gmail.com>
2023-07-24 11:06:12 -0300

    filterCreate modificacion

--- client/src/components/Filters/FilterByCreate.jsx
+++ client/src/components/Filters/FilterByCreate.jsx
@@ -1,30 +1,31 @@
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from "react-redux"
-import { filterByCreate, getAllActivities } from '../../redux/actions'
-import style from "./Filters.module.css"
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterByCreate, getAllActivities } from '../../redux/actions';
+import style from './Filters.module.css';
 
 export default function FilterByCreate() {
+  const dispatch = useDispatch();
+  const activities = useSelector((state) => state.activities);
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getAllActivities());
+  }, [dispatch]);
 
-    const activities = useSelector(state=> state.activities)
-
-    useEffect(()=>{
-        dispatch(getAllActivities())
-    },[dispatch])
-
-    function handleSelect (e){
-        dispatch(filterByCreate(e.target.value)) 
-    };
+  function handleSelect(e) {
+    dispatch(filterByCreate(e.target.value));
+  }
 
   return (
-        <div className={style.select}>
-            <select onChange={(el)=>handleSelect(el)}>
-            <option value ='sin filtros'>Activities</option>
-                {activities.map((act, index)=>(
-            <option key={index} value={act.name}>{act.name}</option>
-            ))}
-            </select>
-        </div>
-    )
-    }
+    <div className={style.select}>
+      <select onChange={(el) => handleSelect(el)}>
+        <option value="sin filtros">Activities</option>
+        {Array.isArray(activities) &&
+          activities.map((act, index) => (
+            <option key={index} value={act.name}>
+              {act.name}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+}
